37ee9bc6d05d7401739b7c66b3f991b0c6aa097a
Yakkala Yagnesh Raghava <yagnesh@live.com>
2012-04-12 17:24:24 +0900

    * refector similar to wrf_install repo

--- download_app.sh
+++ download_app.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 # download apps from env
+
+RESET='\e[0m'
+RED="\E[31m"
+GREEN='\E[32m'
+
+function usage()
+{
+    echo
+    echo "      USAGE: $0 <all|appname>"
+    echo
+    echo "where \"appname\" is one of the follwoing"
+    echo "========================================="
+    for app in ${apps[@]}; do
+        echo $app
+    done
+    exit 4
+}
+
 function download_package()
 {
-    RESET='\e[0m'
-    RED="\E[31m"
-    GREEN='\E[32m'
     echo "Downloading.... $APP"
     wget -c $@ -P $BASE/src
     if [ $? -ne 0 ]; then
@@ -17,114 +32,109 @@ function download_package()
     return 0
 }
 
-function shebang ()
-{
-    file=$1
-    if [ ! -f $file ]; then
-        echo '#!/bin/bash' > $file
-        chmod +x $file
-        echo '#' >> $file       # just an extra comment line
-    fi
-}
-
-function clean_line ()
-{
-    perl -i -ne "print unless m!$1!" $2
-}
-
-function update_util_files()
+function extract_package()
 {
     pack=$1
-   # update generated scripts
-    for x in $util_scr; do
-        clean_line $1 $x
-    done
-
-    echo rm ${APP}.${EXT} \# $pack >> $CLEAN_DOWN
-    echo rm -rf ${DIR} \# $pack >> $CLEAN_SRC
+    appsrc=${APP}.${EXT}
+    extractlog=${APP}.${EXT}.extraction.log
+    logdir=$BASE/src/log
+    cd $BASE/src
+    if [ -d ${DIR} ]; then
+        rm -r ${DIR}        # remove previouly extracted things
+    fi
+    mkdir -p $logdir
     case ${EXT} in
-        *".gz"|*"tgz" )
-            cd $BASE/src
-            tar xzvf ${APP}.${EXT}
-            echo tar xzvf ${APP}.${EXT} \# $pack >> $EXTRACT_SRC ;;
+        *"gz"|*"tgz" )
+            tar xzvf $appsrc &> $logdir/$extractlog &&
+            echo "extraction is done and log is written to \"$logdir/$extractlog\""
+            ;;
         *"zip" )
-            cd $BASE/src
-            unzip ${APP}.${EXT}
-            echo unzip ${APP}.${EXT} \# $pack >> $EXTRACT_SRC ;;
+            unzip $appsrc &> $logdir/$extractlog &&
+            echo "extraction is done and log is written to \"$logdir/$extractlog\""
+            ;;
         * )
-            echo "dont know how to extract this one:" ${APP}.${EXT}
-            echo echo "dont know how to extract this one:" ${APP}.${EXT} >> $EXTRACT_SRC
+            echo  "dont know how to extract this one:" ${APP}.${EXT}
             ;;
     esac
 }
 
 ###########################################################################
 # CODE STARTS FORM HERE
-if [ $# -lt 1 ]
-then
-    echo "${#} arguments."
-    echo "USAGE: $0 <all|app>"
-    exit 4
-fi
-
 export BASE=${BASE:-$WRF_BASE}
-export SCRIPTS_DIR=${SCRIPTS_DIR:-$(pwd)}
 
 # check needed environment variables are present or not
 env_error=24
 if [ -z $BASE ]; then
     echo BASE variable is empty
-    echo please set that to know where to download files
+    echo did you source the SOURCEME file.?
+    echo run \"source SOURCEME\"
     exit $env_error
 elif [ -z $SCRIPTS_DIR ]; then
     echo SCRIPTS_DIR variable is empty
-    echo please set that to know where the env files located
+    echo did you source the SOURCEME file.?
+    echo run \"source SOURCEME\"
     exit $env_error
 fi
 
 mkdir -p $BASE/src      # Setup source directory
 
-# prepare utilitly scripts
-CLEAN_DOWN=$BASE/src/clean_downloads.sh
-CLEAN_SRC=$BASE/src/clean_source.sh
-EXTRACT_SRC=$BASE/src/extract_source.sh
-
-util_scr="$CLEAN_SRC $CLEAN_DOWN $EXTRACT_SRC"
-for f in $util_scr ; do
-    shebang $f
+apps=()
+for envfile in `find $appsdir -maxdepth 1 -type f  -name "*.env"` ; do
+    temp="${envfile##*/}" # strip out path name
+    app="${temp%.[^.]*}" # strip out ".env"
+    apps=(${apps[@]} ${app})
 done
 
+if [ $# -lt 1 ]
+then
+    echo "${#} arguments."
+    usage $0
+fi
+
+
+unset app
 #
 counter=0
 while [ $counter -le $# ]; do
     case $1 in
         all|ALL)
-            for envfile in `find $SCRIPTS_DIR/ -maxdepth 1 -type f  -name "*.env"`
+            for app in ${apps[@]};
             do
                 unset URL
-                envfile="${envfile##*/}" # strip out path name
-                package="${envfile%.[^.]*}" # strip out ".env"
-                . $SCRIPTS_DIR/${envfile} &>/dev/null
+                . $appsdir/${app}.env  &>/dev/null
                 if [ ! -z $URL ]; then
                     download_package $URL
                 fi
-                [ $? == 0 ] && update_util_files $package
+                [ $? == 0 ] && extract_package $package
             done
-            exit 0
             ;;
+
+        -e )
+            shift
+            for app in ${apps[@]};
+            do
+                unset URL
+                . $appsdir/${app}.env  &>/dev/null
+                extract_package $package
+            done
+            ;;
+
         *)
             unset URL
             package="${1%.[^.]*}" # strip out ".env" if available
-            . $SCRIPTS_DIR/${package}.env  &>/dev/null
+            . $appsdir/${package}.env  &>/dev/null
             if [ ! -z $URL ]; then
                 download_package $URL
+            else
+                echo -e "$RED unknown app \"$package\" $RESET"
+                usage $0
+                exit 64
             fi
-            [ $? == 0 ] && update_util_files $package
+            [ $? == 0 ] && extract_package $package
             ;;
     esac
 
     shift
     let counter=counter+1
 done
-
 exit 0
